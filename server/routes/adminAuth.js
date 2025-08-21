18e5c28dfdf6e80798c177f6a198fc6c765be025
Hamidreza <delshad.swdn@gmail.com>
2025-08-21 17:15:26 +0330

    fix some issues in app.js

--- server/routes/adminAuth.js
+++ server/routes/adminAuth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 const ADMIN_JWT_SECRET = process.env.ADMIN_JWT_SECRET;
 
-router.post('/admin/login', (req, res) => {
+router.post('/login', (req, res) => {
   const { password } = req.body;
   console.log(password,"password")
   if (!ADMIN_PASSWORD || password !== ADMIN_PASSWORD) {
